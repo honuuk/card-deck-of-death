5db4b98afd3cc177f07108f79417e7435170a49d
jewook <jewook3617@gmail.com>
2020-10-28 19:02:34 +0900

    add: card images

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,38 +3,6 @@ import { Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 import { CardProps, CardType, EventHandler } from '../types';
 
-const back = require('../public/card_back.png');
-const spade10 = require('../public/spade10.png');
-const spade2 = require('../public/spade2.png');
-const spade3 = require('../public/spade3.png');
-const spade4 = require('../public/spade4.png');
-const spade5 = require('../public/spade5.png');
-const spade6 = require('../public/spade6.png');
-const spade7 = require('../public/spade7.png');
-const spade8 = require('../public/spade8.png');
-const spade9 = require('../public/spade9.png');
-const spadeA = require('../public/spadeA.png');
-const spadeJ = require('../public/spadeJ.png');
-const spadeK = require('../public/spadeK.png');
-const spadeQ = require('../public/spadeQ.png');
-
-const imageMap = {
-  [CardType.back]: back,
-  [CardType.spadeA]: spadeA,
-  [CardType.spade2]: spade2,
-  [CardType.spade3]: spade3,
-  [CardType.spade4]: spade4,
-  [CardType.spade5]: spade5,
-  [CardType.spade6]: spade6,
-  [CardType.spade7]: spade7,
-  [CardType.spade8]: spade8,
-  [CardType.spade9]: spade9,
-  [CardType.spade10]: spade10,
-  [CardType.spadeJ]: spadeJ,
-  [CardType.spadeQ]: spadeQ,
-  [CardType.spadeK]: spadeK,
-};
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -50,8 +18,7 @@ const styles = StyleSheet.create({
 });
 
 const Card = (props: CardProps) => {
-  const { selectedCard, startTimer, addCount } = props;
-
+  const { source, startTimer, addCount } = props;
   const handlePress: EventHandler = () => {
     if (startTimer && addCount) {
       startTimer();
@@ -62,9 +29,9 @@ const Card = (props: CardProps) => {
   return (
     <TouchableOpacity
       style={styles.container}
-      activeOpacity={selectedCard === CardType.back ? 0.9 : 1}
+      activeOpacity={startTimer && addCount ? 0.9 : 1}
       onPress={handlePress}>
-      <Image style={styles.image} source={imageMap[selectedCard]} resizeMode='contain' />
+      <Image style={styles.image} source={source} resizeMode='contain' />
     </TouchableOpacity>
   );
 };
--- components/Work.tsx
+++ components/Work.tsx
@@ -4,6 +4,116 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { CardType, WorkProps } from '../types';
 import Card from './Card';
 
+const clubs10 = require('../public/10_of_clubs.png');
+const diamonds10 = require('../public/10_of_diamonds.png');
+const hearts10 = require('../public/10_of_hearts.png');
+const spades10 = require('../public/10_of_spades.png');
+const clubs2 = require('../public/2_of_clubs.png');
+const diamonds2 = require('../public/2_of_diamonds.png');
+const hearts2 = require('../public/2_of_hearts.png');
+const spades2 = require('../public/2_of_spades.png');
+const clubs3 = require('../public/3_of_clubs.png');
+const diamonds3 = require('../public/3_of_diamonds.png');
+const hearts3 = require('../public/3_of_hearts.png');
+const spades3 = require('../public/3_of_spades.png');
+const clubs4 = require('../public/4_of_clubs.png');
+const diamonds4 = require('../public/4_of_diamonds.png');
+const hearts4 = require('../public/4_of_hearts.png');
+const spades4 = require('../public/4_of_spades.png');
+const clubs5 = require('../public/5_of_clubs.png');
+const hearts5 = require('../public/5_of_hearts.png');
+const clubs6 = require('../public/6_of_clubs.png');
+const clubs7 = require('../public/7_of_clubs.png');
+const clubs8 = require('../public/8_of_clubs.png');
+const clubs9 = require('../public/9_of_clubs.png');
+const spades5 = require('../public/5_of_spades.png');
+const hearts6 = require('../public/6_of_hearts.png');
+const spades6 = require('../public/6_of_spades.png');
+const hearts7 = require('../public/7_of_hearts.png');
+const spades7 = require('../public/7_of_spades.png');
+const hearts8 = require('../public/8_of_hearts.png');
+const spades8 = require('../public/8_of_spades.png');
+const diamonds9 = require('../public/9_of_diamonds.png');
+const hearts9 = require('../public/9_of_hearts.png');
+const spades9 = require('../public/9_of_spades.png');
+const clubsA = require('../public/ace_of_clubs2.png');
+const diamondsA = require('../public/ace_of_diamonds2.png');
+const heartsA = require('../public/ace_of_hearts2.png');
+const spadesA = require('../public/ace_of_spades2.png');
+const back = require('../public/card_back.png');
+const clubsJ = require('../public/jack_of_clubs2.png');
+const diamondsJ = require('../public/jack_of_diamonds2.png');
+const heartsJ = require('../public/jack_of_hearts2.png');
+const spadesJ = require('../public/jack_of_spades2.png');
+const clubsK = require('../public/king_of_clubs2.png');
+const diamondsK = require('../public/king_of_diamonds2.png');
+const heartsK = require('../public/king_of_hearts2.png');
+const spadesK = require('../public/king_of_spades2.png');
+const heartsQ = require('../public/queen_of_hearts2.png');
+const spadesQ = require('../public/queen_of_spades2.png');
+const clubsQ = require('../public/queen_of_clubs2.png');
+const diamonds5 = require('../public/5_of_diamonds.png');
+const diamonds6 = require('../public/6_of_diamonds.png');
+const diamonds7 = require('../public/7_of_diamonds.png');
+const diamonds8 = require('../public/8_of_diamonds.png');
+const diamondsQ = require('../public/queen_of_diamonds2.png');
+
+const imageMap = {
+  [CardType.back]: back,
+  [CardType.spadesA]: spadesA,
+  [CardType.spades2]: spades2,
+  [CardType.spades3]: spades3,
+  [CardType.spades4]: spades4,
+  [CardType.spades5]: spades5,
+  [CardType.spades6]: spades6,
+  [CardType.spades7]: spades7,
+  [CardType.spades8]: spades8,
+  [CardType.spades9]: spades9,
+  [CardType.spades10]: spades10,
+  [CardType.spadesJ]: spadesJ,
+  [CardType.spadesQ]: spadesQ,
+  [CardType.spadesK]: spadesK,
+  [CardType.clubsA]: clubsA,
+  [CardType.clubs2]: clubs2,
+  [CardType.clubs3]: clubs3,
+  [CardType.clubs4]: clubs4,
+  [CardType.clubs5]: clubs5,
+  [CardType.clubs6]: clubs6,
+  [CardType.clubs7]: clubs7,
+  [CardType.clubs8]: clubs8,
+  [CardType.clubs9]: clubs9,
+  [CardType.clubs10]: clubs10,
+  [CardType.clubsJ]: clubsJ,
+  [CardType.clubsQ]: clubsQ,
+  [CardType.clubsK]: clubsK,
+  [CardType.heartsA]: heartsA,
+  [CardType.hearts2]: hearts2,
+  [CardType.hearts3]: hearts3,
+  [CardType.hearts4]: hearts4,
+  [CardType.hearts5]: hearts5,
+  [CardType.hearts6]: hearts6,
+  [CardType.hearts7]: hearts7,
+  [CardType.hearts8]: hearts8,
+  [CardType.hearts9]: hearts9,
+  [CardType.hearts10]: hearts10,
+  [CardType.heartsJ]: heartsJ,
+  [CardType.heartsQ]: heartsQ,
+  [CardType.heartsK]: heartsK,
+  [CardType.diamondsA]: diamondsA,
+  [CardType.diamonds2]: diamonds2,
+  [CardType.diamonds3]: diamonds3,
+  [CardType.diamonds4]: diamonds4,
+  [CardType.diamonds5]: diamonds5,
+  [CardType.diamonds6]: diamonds6,
+  [CardType.diamonds7]: diamonds7,
+  [CardType.diamonds8]: diamonds8,
+  [CardType.diamonds9]: diamonds9,
+  [CardType.diamonds10]: diamonds10,
+  [CardType.diamondsJ]: diamondsJ,
+  [CardType.diamondsQ]: diamondsQ,
+  [CardType.diamondsK]: diamondsK,
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -169,8 +279,8 @@ const Work = (props: WorkProps) => {
             <Text style={styles.result}>Fail</Text>
           ) : (
             <>
-              <Card selectedCard={CardType.back} startTimer={startTimer} addCount={addCount} />
-              <Card selectedCard={CardType.spadeK} />
+              <Card source={imageMap[CardType.back]} startTimer={startTimer} addCount={addCount} />
+              <Card source={imageMap[CardType.spade3]} />
             </>
           )}
         </View>
