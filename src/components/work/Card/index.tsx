fbb91c20e07dee133b19ab284b529506ccb5f6c1
jewook <jewook3617@gmail.com>
2021-03-17 19:02:18 +0900

    add: image render function

--- src/components/work/Card/index.tsx
+++ src/components/work/Card/index.tsx
@@ -7,12 +7,17 @@ import S from './style';
 const Card = (props: CardProps) => {
   const { source, handlePress } = props;
 
+  const renderImage = () => {
+    if (!source) return null;
+    return <Image style={S.image} source={source} resizeMode='contain' />;
+  };
+
   return (
     <TouchableOpacity
       style={S.container}
       activeOpacity={handlePress ? 0.9 : 1}
       onPress={handlePress}>
-      {source && <Image style={S.image} source={source} resizeMode='contain' />}
+      {renderImage()}
     </TouchableOpacity>
   );
 };
