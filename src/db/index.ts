0ff609bcb602c43616bc2c590466bc6fcbdfc9bf
jewook <jewook3617@gmail.com>
2021-05-04 10:02:07 +0900

    add firestore config

---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export default db;
