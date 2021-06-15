2d22a0f0773bae84454b57ceb2c705e3f55cd829
jewook <jewook3617@gmail.com>
2021-06-15 11:25:17 +0900

    안 쓰는 코드 제거

--- src/containers/LandingContainer/index.tsx
+++ src/containers/LandingContainer/index.tsx
@@ -1,6 +1,6 @@
 import { useFonts } from '@expo-google-fonts/inter';
 import React, { useState } from 'react';
-import { Alert, Modal, Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 import { LandingContainerProps } from '../../../types';
@@ -11,7 +11,6 @@ const LandingContainer = (props: LandingContainerProps) => {
   const { navigation } = props;
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [fontsLoaded] = useFonts({
-    DotGothic16: require('../../../assets/fonts/DotGothic16-Regular.ttf'),
     'NotoSansKR-Black': require('../../../assets/fonts/NotoSansKR-Black.otf'),
     'NotoSansKR-Bold': require('../../../assets/fonts/NotoSansKR-Bold.otf'),
     'NotoSansKR-Light': require('../../../assets/fonts/NotoSansKR-Light.otf'),
@@ -35,7 +34,6 @@ const LandingContainer = (props: LandingContainerProps) => {
         <Text style={S.howToIcon}>?</Text>
       </TouchableOpacity>
       <View style={S.TitleWrap}>
-        {/* <Text style={S.gameTitleKo}>죽음의 카드덱</Text> */}
         <Animatable.Text
           style={S.gameTitleEn}
           animation='slideInDown'
@@ -43,12 +41,9 @@ const LandingContainer = (props: LandingContainerProps) => {
           direction='alternate'>
           Card
         </Animatable.Text>
-        {/* <Text style={S.gameTitleEn}>Card</Text> */}
-        {/* <Text style={{ ...S.gameTitleEn, ...S.gameTitleOf }}>of</Text> */}
         <Animatable.Text style={{ ...S.gameTitleEn, ...S.gameTitleDeath }} animation='bounceIn'>
           Squat
         </Animatable.Text>
-        {/* <Text style={{ ...S.gameTitleEn, ...S.gameTitleDeath }}>Squat</Text> */}
       </View>
       <TouchableOpacity style={S.button} onPress={() => navigation.navigate('Work')}>
         <Text style={S.buttonTextgmd}>Play Now</Text>
