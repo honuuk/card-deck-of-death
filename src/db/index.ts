437451113714766b8200d27988fcf55beadf1c50
jewook <jewook3617@gmail.com>
2021-06-26 14:00:45 +0900

    firebase config에서 불필요한 항목 제거

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,11 @@
-import { API_KEY, AUTH_DOMAIN, DATABASE_URL, PROJECT_ID, MESSAGING_SENDER_ID, APP_ID } from '@env';
+import { API_KEY, DATABASE_URL, PROJECT_ID, APP_ID } from '@env';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
   databaseURL: DATABASE_URL,
   projectId: PROJECT_ID,
-  messagingSenderId: MESSAGING_SENDER_ID,
   appId: APP_ID,
 };
 
